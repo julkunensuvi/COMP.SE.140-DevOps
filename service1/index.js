3d73b07317fb220fc62846dbddf9db86d2f3f1c9
Suvi <suvi.julkunen@tuni.fi>
2024-10-05 12:34:10 +0300

    Dummy services and initial dockerization

---
+++ service1/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 8199;
+
+function getInfo() {
+    return {
+        ip_address: 'Dummy',
+        processes: 'Dummy',
+        disk_space: 'Dummy',
+        uptime: 'Dummy'
+    };
+}
+
+app.get('/info', async (req, res) => {
+    const service1Info = getInfo();
+    
+    try {
+        const service2Info = await axios.get('http://service2:8200/info');
+        res.json({
+            service1: service1Info,
+            service2: service2Info.data
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to connect to Service2' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Service1 is running on port ${PORT}`);
+});
---
+++ service2/src/main.rs
@@ -0,0 +1,40 @@
+#[macro_use] extern crate rocket;
+use rocket::serde::json::Json;
+use std::process::Command;
+
+#[derive(serde::Serialize)]
+struct Info {
+    ip_address: String,
+    processes: String,
+    disk_space: String,
+    uptime: String,
+}
+
+fn get_info() -> Info {
+    let ip_address = "dummy".to_string();
+    let processes =  "dummy".to_string();
+    let disk_space =  "dummy".to_string();
+    let uptime =  "dummy".to_string();
+
+    Info {
+        ip_address,
+        processes,
+        disk_space,
+        uptime,
+    }
+}
+
+#[get("/info")]
+fn info() -> Json<Info> {
+    println!("INFO: /info endpoint called");
+    Json(get_info())
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![info])
+        .configure(rocket::Config {
+            port: 8200,
+            ..rocket::Config::default()
+        })
+}
