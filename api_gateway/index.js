5377d7de1dfef2c4afc0ac07cc45f539c4005a8d
Suvi <suvi.julkunen@tuni.fi>
2025-01-12 18:46:10 +0200

    Implement /state API path

--- api_gateway/index.js
+++ api_gateway/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = 8197;
+let state = 'INIT';
 
-app.get('/state', (req, res) => {
-    res.type('text/plain').send("TODO");
+function isAuthenticated(req) {
+    const authHeader = req.headers['authorization'];
+    return !!authHeader; // Return true if Authorization header is present
+}
+
+app.use((req, res, next) => {
+    // If the system is in INIT state and the user is authenticated, transition to RUNNING
+    if (state === 'INIT' && isAuthenticated(req)) {
+        console.log('User authenticated. INIT to RUNNING.');
+        state = 'RUNNING';
+    }
+    next();
 });
 
-// Endpoint to update the state
-app.put('/state', (req, res) => {
-    res.status(200).send(`TODO`);
+app.get('/state', (req, res) => {
+    res.type('text/plain').send(state);
 });
 
 // Endpoint to handle a request
