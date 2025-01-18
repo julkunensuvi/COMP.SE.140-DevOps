014821967014fe12320328065f52483f8567a969
Suvi <suvi.julkunen@tuni.fi>
2025-01-18 19:07:05 +0200

    Add nginx logs and mount them

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 echo "Access system at:  http://localhost:8198)"
+# Ensure the log directory exists
+mkdir -p /var/log/nginx
+
+# Create empty log files
+touch /var/log/nginx/access.log
 nginx -g "daemon off;"
