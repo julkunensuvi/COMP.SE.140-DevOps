48472cc6c3392150ef4939f944725d9c296094d9
Suvi <suvi.julkunen@tuni.fi>
2025-01-19 14:05:14 +0200

    PUT state transitions, edit initial stae test with initial transfer to INIT state

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,16 +8,29 @@ const AUTH_CREDENTIALS = {
     password: 'Kahvikuppi',
 };
 
+
+
 describe('GET /state', () => {
     it('should return the initial state as INIT', async () => {
-        const res = await axios.get(`${BASE_URL}/state`, {
+
+        const res = await axios.put(`${BASE_URL}/state`, { state: 'INIT' }, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const res2 = await axios.get(`${BASE_URL}/state`, {
             headers: {
                 'Accept': 'text/plain',
             },
         });
-        expect(res.status).toBe(200);
-        expect(res.data).toBe('INIT');
+        expect(res2.status).toBe(200);
+        expect(res2.data).toBe('RUNNING');
+
     });
+});
+
+describe('State transitions', () => {
 
     it('should update state to RUNNING after processing a valid log entry with Basic Auth', async () => {
         // First request processes the log and updates the state
@@ -34,5 +47,46 @@ describe('GET /state', () => {
         });
         expect(res2.status).toBe(200);
         expect(res2.data).toBe('RUNNING');
+    });
+
+    it('should update state using PUT /state', async () => {
+        // ensure user is authenicated > state is RUNNING
+        const res1 = await axios.get(`${BASE_URL_SERVICE1}/api/`, {
+            headers: {
+                'Accept': 'text/plain',
+            },
+            auth: AUTH_CREDENTIALS,
+        });
+        // Transition to PAUSED
+        let res = await axios.put(`${BASE_URL}/state`, { state: 'PAUSED' }, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        expect(res.status).toBe(200);
+
+        res = await axios.get(`${BASE_URL}/state`, {
+            headers: {
+                'Accept': 'text/plain',
+            },
+        });
+        expect(res.status).toBe(200);
+        expect(res.data).toBe('PAUSED');
+
+        // Transition to RUNNING
+        res = await axios.put(`${BASE_URL}/state`, { state: 'RUNNING' }, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        expect(res.status).toBe(200);
+
+        res = await axios.get(`${BASE_URL}/state`, {
+            headers: {
+                'Accept': 'text/plain',
+            },
+        });
+        expect(res.status).toBe(200);
+        expect(res.data).toBe('RUNNING'); 
     });
 });
